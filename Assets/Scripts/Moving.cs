d53c4108f682225d74c3c1de8a97dabd0ef00828
Yurii Vasylchenko <64155818+KaiKutar0@users.noreply.github.com>
2022-11-15 00:56:48 +0200

    Added Rotation and Movement

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -1,19 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody m_Rigidbody;
+    public float m_Speed = 5f;
+
     void Start()
     {
-        //Lexaaaa priveeet
-        int a=5;
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Store user input as a movement vector
+        Vector3 m_Input = new Vector3(-1 * Input.GetAxis("Horizontal"), 0, -1 * Input.GetAxis("Vertical"));
+
+        //Apply the movement vector to the current position, which is
+        //multiplied by deltaTime and speed for a smooth MovePosition
+        m_Rigidbody.MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
     }
 }
