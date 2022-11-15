7d63b557a2c7c725cf7c5281cc924f01f6541d35
NyaNyaNyuNyu <alekseypshenichnyu@gmail.com>
2022-11-15 23:18:17 +0100

    Update Jump.cs

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
    public Vector3 jump;
-    public float jumpForce = 2.0f;
+    public float jumpForce = 4.0f;
 
     public bool isGrounded;
     Rigidbody rb;
     void Start(){
         rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        jump = new Vector3(0.0f, 1.0f, 0.0f);
     }
 
     void OnCollisionStay(){
